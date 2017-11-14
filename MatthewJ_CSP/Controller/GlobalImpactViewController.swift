bfe0f3086c0b8c639be1020799f0e83a3a2cbe5e
mattitude17 <31513806+mattitude17@users.noreply.github.com>
2017-11-14 07:33:06 -0700

    Started creating the framework for the global impact page of the app.

--- MatthewJ_CSP/Controller/GlobalImpactViewController.swift
+++ MatthewJ_CSP/Controller/GlobalImpactViewController.swift
@@ -7,9 +7,43 @@
 //
 
 import UIKit
+import AVFoundation
 
 class GlobalImpactViewController: UIViewController
 {
+    private var imageCounter : Int = 0
+    
+    @IBOutlet weak var imageFrame: UIImageView!
+    @IBOutlet weak var changeInformation: UIButton!
+    
+    @IBAction func changePicture() -> Void
+    {
+        switchImage()
+    }
+    
+    private func switchImage() -> Void
+    {
+        if (imageCounter > 2)
+        {
+            imageCounter = 0
+        }
+        
+        if (imageCounter == 0)
+        {
+            imageFrame.image = UIImage(named: "aidsRibbon")
+        }
+        else if (imageCounter == 1)
+        {
+            imageFrame.image = UIImage(named: "appDesign")
+        }
+        else
+        {
+            imageFrame.image = UIImage(named: "")
+        }
+        
+        imageCounter += 1
+    }
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
