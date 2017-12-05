29adc23eba084e60c1718a2ca29eca36437046bc
mattitude17 <31513806+mattitude17@users.noreply.github.com>
2017-12-05 09:10:47 -0700

    Added code for the happy tree drawing.

--- MatthewJ_CSP/View/DrawingView.swift
+++ MatthewJ_CSP/View/DrawingView.swift
@@ -16,31 +16,61 @@ public class DrawingView: UIView
     {
         //Drawing code
         drawStickFigure().stroke()
-        drawTurtle()
-        drawHappyTree()
+        //drawTurtle()
+        //drawHappyTree()
     }
-}
 
-private func drawStickFigure() -> UIBezierPath
-{
-    let stickFigure = UIBezierPath()
+    private func drawStickFigure() -> UIBezierPath
+    {
+        let stickFigure = UIBezierPath()
     
-    UIColor.green.setStroke()
-    stickFigure.lineWidth = 3.0
+        UIColor.green.setStroke()
+        stickFigure.lineWidth = 3.0
     
-    stickFigure.addArc(withCenter: CGPoint(x: 200,y:200),
-                       radius: CGFloat(20),
-                       startAngle: CGFloat(0),
-                       endAngle: CGFloat(2) * CGFloat.pi,
-                       clockwise: true)
-    stickFigure.move(to: CGPoint(x: 200,y: 220))
-    stickFigure.addLine(to: CGPoint(x: 200,y: 270))
-    stickFigure.move(to: CGPoint(x: 180,y: 240))
-    stickFigure.addLine(to: CGPoint(x: 220,y: 240))
-    stickFigure.move(to: CGPoint(x: 200,y: 270))
-    stickFigure.addLine(to: CGPoint(x: 180,y: 300))
-    stickFigure.move(to: CGPoint(x: 200,y: 270))
-    stickFigure.addLine(to: CGPoint(x: 220,y: 300))
+        stickFigure.addArc(withCenter: CGPoint(x: 200,y:200),
+                           radius: CGFloat(20),
+                           startAngle: CGFloat(0),
+                           endAngle: CGFloat(2) * CGFloat.pi,
+                           clockwise: true)
+        stickFigure.move(to: CGPoint(x: 200,y: 220))
+        stickFigure.addLine(to: CGPoint(x: 200,y: 270))
+        stickFigure.move(to: CGPoint(x: 180,y: 240))
+        stickFigure.addLine(to: CGPoint(x: 220,y: 240))
+        stickFigure.move(to: CGPoint(x: 200,y: 270))
+        stickFigure.addLine(to: CGPoint(x: 180,y: 300))
+        stickFigure.move(to: CGPoint(x: 200,y: 270))
+        stickFigure.addLine(to: CGPoint(x: 220,y: 300))
     
-    return stickFigure
+        return stickFigure
+    }
+    
+    public func drawHappyTree() -> Void
+    {
+        let bobRoss = UIBezierPath()
+        
+        bobRoss.move(to: CGPoint(x:80, y:50))
+        bobRoss.addLine(to: CGPoint(x:120, y:150))
+        bobRoss.addLine(to: CGPoint(x:40, y:150))
+        bobRoss.close()
+        UIColor(patternImage: UIImage(named: "tree")!).setFill()
+        UIColor.brown.setStroke()
+        bobRoss.lineWidth = 2.0
+        bobRoss.fill()
+        bobRoss.stroke()
+        
+        let happyTree = UIBezierPath()
+        
+        UIColor.green.setFill()
+        happyTree.move(to: CGPoint(x: 110,y: 150))
+        happyTree.addLine(to: CGPoint(x: 150,y: 200))
+        happyTree.addLine(to: CGPoint(x: 10,y: 200))
+        happyTree.addLine(to: CGPoint(x: 50,y: 150))
+        happyTree.close()
+        happyTree.stroke()
+        happyTree.fill()
+        happyTree.move(to: CGPoint(x: 80,y: 200))
+        happyTree.addLine(to: CGPoint(x: 80,y: 250))
+        happyTree.lineWidth = 6.0
+        happyTree.stroke()
+    }
 }
