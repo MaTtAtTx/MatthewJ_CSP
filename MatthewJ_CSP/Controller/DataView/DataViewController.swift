88cebc958a4402f7d8c065dff34f0654a1ef0947
mattitude17 <31513806+mattitude17@users.noreply.github.com>
2018-01-05 09:40:43 -0700

    Added files for the dataViewController.

---
+++ MatthewJ_CSP/Controller/DataView/DataViewController.swift
@@ -0,0 +1,42 @@
+//
+//  DataViewController.swift
+//  MatthewJ_CSP
+//
+//  Created by Johnsen, Matthew on 1/5/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UITableViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int
+    {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+}
---
+++ MatthewJ_CSP/Model/BucketItem.swift
@@ -0,0 +1,9 @@
+//
+//  BucketItem.swift
+//  MatthewJ_CSP
+//
+//  Created by Johnsen, Matthew on 1/5/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import Foundation
